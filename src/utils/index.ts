2f7df3a4f87e1a7649a63d95f28a826b3b19d872
Philip <106172533+yankes404@users.noreply.github.com>
2025-04-05 17:25:25 +0200

    Added getResourceName

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { fetchNui } from "./fetch-nui";
 export { isProduction } from "./is-production";
 export { openUrl } from "./open-url";
+export { getResourceName } from "./get-resource-name";
